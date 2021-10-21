54c142f06c976c6a161c5d4640ae4aacb40b1153
rumigor <rumigor@gmail.com>
2021-10-21 11:43:55 +0300

    скрыл api_key

--- app/src/main/java/ru/rumigor/drinks/data/api/DrinksApiInterceptor.kt
+++ app/src/main/java/ru/rumigor/drinks/data/api/DrinksApiInterceptor.kt
@@ -3,16 +3,17 @@ package ru.rumigor.drinks.data.api
 
 import okhttp3.Interceptor
 import okhttp3.Response
-
+import ru.rumigor.drinks.BuildConfig.API_KEY
 
 object DrinksApiInterceptor : Interceptor {
+
 
     override fun intercept(chain: Interceptor.Chain): Response =
         chain.proceed(
             chain.request()
                 .newBuilder()
                 .addHeader("x-rapidapi-host", "the-cocktail-db.p.rapidapi.com")
-                .addHeader("x-rapidapi-key", "2d55321602msh33c8d91c041d686p1bd305jsn6b6ee598e59a")
+                .addHeader("x-rapidapi-key", API_KEY)
                 .build()
         )
 
